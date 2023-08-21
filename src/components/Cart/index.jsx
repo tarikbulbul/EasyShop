383c91caef5a87054087373e8f49a89b0d973a2d
Tarik Bulbul <bulbultarik@hotmail.com>
2023-08-21 16:35:57 +0300

    finish checkout component design

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -37,7 +37,7 @@ const CartContent = () => {
                 </TableContainer>
             ) : (
                 <div className="p-4">
-                    <p className="text-center text-xl text-red-500">Oops your cart is empty 😟</p>
+                    <p className="text-center text-xl text-red-500">Oops your cart is empty 😬</p>
                 </div>
             )}
         </div>
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,17 +1,20 @@
-import { Button } from "@chakra-ui/react";
+import { Button } from '@chakra-ui/react';
 import CartContent from '../../components/Cart';
 import Checkout from '../../components/Cart/Checkout';
 import { Link } from 'react-router-dom';
 import { BsArrowLeft } from 'react-icons/bs';
+import { useSelector } from 'react-redux';
+import { productsSelector } from '../../features/products/productsSlice';
 
 const cart = () => {
+    const { cart } = useSelector(productsSelector);
     return (
         <div className="max-w-[1024px] mx-auto min-h-screen flex justify-center items-start flex-col space-y-6">
             <Link to="/">
                 <Button className="!font-Poppins" leftIcon={<BsArrowLeft />}>Go Home</Button>
             </Link>
             <CartContent />
-            <Checkout />
+            {cart.length > 0 && <Checkout />}
         </div>
     );
 };
